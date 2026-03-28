01e832b3feec92d27baea6a1219096a18349e772
Happy-S5 <littleejido@gmail.com>
2026-03-28 14:42:46 -0700

    Enhance makeCacheMatrix with set/get functions
    Added functions to set and get matrix and its inverse in makeCacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
 
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+
+        get <- function() x
+
+        setinverse <- function(inverse) inv <<- inverse
+
+        getinverse <- function() inv
+
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+x <- matrix(c(1, 2, 3,
+              0, 1, 4,
+              5, 6, 0), nrow = 3, byrow = TRUE)
+y <- makeCacheMatrix(x)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+cacheSolve(y)
